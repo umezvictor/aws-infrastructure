d8e464ca37d9fb75a95c945bdac2a53e6563890c
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2026-01-28 13:24:11 +0200

    dev

--- main.tf
+++ main.tf
@@ -21,39 +21,34 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-#ssl certificate - ssl
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = "eventsenta.com"
-#   validation_method = "DNS"
-
-#   tags = {
-#     Environment = "production"
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
+# module "vpc" {
+#   source = "./modules/vpc"
+#   cidr   = var.cidr
 # }
 
-
-# resource "aws_route53_record" "hello_cert_dns" {
-#   allow_overwrite = true
-#   name =  tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
-#   records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
-#   type = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
-#   zone_id = aws_route53_zone.eventsenta.zone_id
-#   ttl = 60
+# module "internal_alb_security_group" {
+#   source = "./modules/security-group"
+#   vpc_id = module.vpc.vpc_id
+#   cidr   = module.vpc.cidr
 # }
 
-# resource "aws_acm_certificate_validation" "api_cert_validation" {
-#   certificate_arn = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [aws_route53_record.hello_cert_dns.fqdn]
+# module "alb" {
+#   source            = "./modules/alb"
+#   name              = "image-resizer-load-balancer"
+#   internal          = false
+#   security_group_id = module.internal_alb_security_group.alb_security_group
+#   subnet_ids = [
+#     module.vpc.public_subnet_id,
+#     module.vpc.private_subnet_id
+#   ]
+#   environment = "production"
 # }
+
 
 resource "aws_route53_zone" "eventsenta" {
   name = "eventsenta.com"
 }
-
+#add the 4 ns records to domain server
 resource "aws_acm_certificate" "certificate" {
   domain_name               = "eventsenta.com"
   subject_alternative_names = ["www.eventsenta.com", "*.eventsenta.com"]
@@ -86,4 +81,17 @@ resource "aws_route53_record" "acm-records" {
 resource "aws_acm_certificate_validation" "acm-validation" {
   certificate_arn         = aws_acm_certificate.certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.acm-records : record.fqdn]
+}
+
+# Alias A record for EB
+resource "aws_route53_record" "api" {
+  zone_id = aws_route53_zone.eventsenta.zone_id
+  name    = "api"
+  type    = "A"
+
+  alias {
+    name                   = "production.eba-bq9phdms.eu-north-1.elasticbeanstalk.com"
+    zone_id                = "Z23GO28BZ5AETM" #zone id of elasticbeanstalk see https://docs.aws.amazon.com/general/latest/gr/elasticbeanstalk.html
+    evaluate_target_health = true
+  }
 }
