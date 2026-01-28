cbdf2c950f4225b9e1e1d1a325d3565a3eee5162
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2026-01-28 12:29:09 +0200

    dev

--- main.tf
+++ main.tf
@@ -64,24 +64,24 @@ resource "aws_acm_certificate" "certificate" {
   }
 }
 
-# resource "aws_route53_record" "acm-records" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+resource "aws_route53_record" "acm-records" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records = [
-#     each.value.record
-#   ]
-#   ttl     = 60
-#   type    = each.value.type
-#   zone_id = aws_route53_zone.eventsenta.zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records = [
+    each.value.record
+  ]
+  ttl     = 60
+  type    = each.value.type
+  zone_id = aws_route53_zone.eventsenta.zone_id
+}
 
 # resource "aws_acm_certificate_validation" "acm-validation" {
 #   certificate_arn         = aws_acm_certificate.certificate.arn
